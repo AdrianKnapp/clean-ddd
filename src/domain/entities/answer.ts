758fba4b8d424220e995a5bdd9e248a0631132a5
AdrianKnapp <adrknapp@gmail.com>
2023-10-02 08:59:20 -0300

    feat: added answer repository

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,11 +1,25 @@
 import { randomUUID } from "crypto";
 
+interface AnswerProps {
+  questionId: string;
+  authorId: string;
+  content: string;
+}
+
 export class Answer {
   public id: string;
+  public questionId: string;
+  public authorId: string;
   public content: string;
 
-  constructor(content: string, id?: string) {
+  constructor({
+    authorId,
+    questionId,
+    content
+  }: AnswerProps, id?: string) {
     this.id = id ?? randomUUID();
+    this.questionId = questionId;
+    this.authorId = authorId;
     this.content = content;
   }
 }
--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,12 +1,24 @@
 import { randomUUID } from "crypto";
 
+interface QuestionProps {
+  authorId: string;
+  title: string;
+  content: string;
+}
+
 export class Question {
   public id: string;
+  public authorId: string;
   public title: string;
   public content: string;
 
-  constructor(title: string, content: string, id?: string) {
+  constructor({
+    authorId,
+    title,
+    content
+  }: QuestionProps, id?: string) {
     this.id = id ?? randomUUID();
+    this.authorId = authorId;
     this.title = title;
     this.content = content;
   }
