691d525bae1073f7df08259d83501bebea28a053
AdrianKnapp <adrknapp@gmail.com>
2023-10-03 08:11:30 -0300

    feat: added value-object for slug typing

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,8 +1,10 @@
 import { randomUUID } from "crypto";
+import { Slug } from "./value-objects/slug";
 
 interface QuestionProps {
   authorId: string;
   title: string;
+  slug: Slug;
   content: string;
 }
 
@@ -10,16 +12,19 @@ export class Question {
   public id: string;
   public authorId: string;
   public title: string;
+  public slug: Slug;
   public content: string;
 
   constructor({
     authorId,
     title,
+    slug,
     content
   }: QuestionProps, id?: string) {
     this.id = id ?? randomUUID();
     this.authorId = authorId;
     this.title = title;
+    this.slug = slug;
     this.content = content;
   }
 }
