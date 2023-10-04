7de48fd5ca03b82a350a8d832af5ffe54bc65f7b
AdrianKnapp <adrknapp@gmail.com>
2023-10-04 08:03:46 -0300

    feat: improved entities props

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Slug } from "./value-objects/slug";
 import { Entity } from "../../core/entities/entity";
+import { UniqueEntityID } from "../../core/entities/unique-entity-id";
 
 interface QuestionProps {
-  authorId: string;
-  title: string;
+  authorId: UniqueEntityID;
+  bestAnswerId?: UniqueEntityID;
   slug: Slug;
+  title: string;
   content: string;
+  createdAt: Date;
+  updatedAt?: Date;
 }
 
 export class Question extends Entity<QuestionProps> {
