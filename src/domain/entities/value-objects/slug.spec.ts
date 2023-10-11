2a4ff92f63be6cefdece33f3757ca1b162943418
AdrianKnapp <adrknapp@gmail.com>
2023-10-11 07:45:29 -0300

    styles: added eslint and fixed code lint issues

--- src/domain/entities/value-objects/slug.spec.ts
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -1,7 +1,7 @@
-import { Slug } from "./slug";
+import { Slug } from './slug'
 
-test("should be able to create a new slug from text", () => {
-  const slug = Slug.createFromText("An example of slug");
+test('should be able to create a new slug from text', () => {
+  const slug = Slug.createFromText('An example of slug')
 
-  expect(slug.value).toBe("an-example-of-slug");
+  expect(slug.value).toBe('an-example-of-slug')
 })
